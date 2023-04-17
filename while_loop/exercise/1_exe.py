41318edd74524040eba4cf0dc8cc185ea58a7d84
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-17 21:25:50 +0300

    Create 1_exe.py

---
+++ while_loop/exercise/1_exe.py
@@ -0,0 +1,7 @@
+numbers = int(input("Enter numbers: "))  # 123456789
+
+while numbers:
+    last_number = numbers % 10  # take last digit
+    print(f"|{last_number}|", end='')  # result: |9||8||7||6||5||4||3||2||1|
+
+    numbers //= 10  # remove last digit
