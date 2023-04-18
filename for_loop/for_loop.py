169a4a576b719f1e976bedc8c3662772236b460b
hellhound <114022856+Emil-Petkov@users.noreply.github.com>
2023-04-18 16:31:31 +0300

    Update for_loop.py

--- for_loop/for_loop.py
+++ for_loop/for_loop.py
@@ -9,12 +9,38 @@
 #     print(s, "->", len(s), end=" ")  # White -> 5 Green -> 5 Red -> 3
 #
 ##########################################################################
-
-num = int(input("Enter Fibonacci sequence: "))
-
-a, b = 1, 1
-
-for _ in range(num):
-    print(a, end=", ")
-    result = a, b = b, a + b
-
+#
+# num = int(input("Enter Fibonacci sequence: "))
+#
+# a, b = 1, 1
+#
+# for _ in range(num):
+#     print(a, end=", ")
+#     result = a, b = b, a + b
+#
+##########################################################################
+#
+# txt = input("Enter text: ").lower()  # Hello world
+#
+# search_letters = ["e", "p", "f", "o", "h", " "]
+#
+# for el in txt:
+#     if el in search_letters:
+#         print(f"({el}) -> in text.")
+#     else:
+#         print(f"({el}) -> not in text.")
+#
+# (h) -> in text.
+# (e) -> in text.
+# (l) -> not in text.
+# (l) -> not in text.
+# (o) -> in text.
+# ( ) -> in text.
+# (w) -> not in text.
+# (o) -> in text.
+# (r) -> not in text.
+# (l) -> not in text.
+# (d) -> not in text.
+#
+###########################################################################
+#
