7bc4e3ca5b8d0b1fc9df26ab2333bc77893b89b0
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-09-04 13:35:09 +0300

    .

---
+++ book_one/turtles_of_the_poseidon/demo.py
@@ -0,0 +1,36 @@
+import turtle
+
+first_turtle = turtle.Turtle()
+second_turtle = turtle.Turtle()
+third_turtle = turtle.Turtle()
+forth_turtle = turtle.Turtle()
+
+################################
+first_turtle.forward(180)
+first_turtle.left(90)
+first_turtle.forward(65)
+first_turtle.right(90)
+first_turtle.forward(100)
+
+################################
+second_turtle.forward(200)
+second_turtle.left(90)
+second_turtle.forward(30)
+second_turtle.right(90)
+second_turtle.forward(55)
+
+################################
+third_turtle.forward(200)
+third_turtle.right(90)
+third_turtle.forward(30)
+third_turtle.left(90)
+third_turtle.forward(55)
+
+################################
+forth_turtle.forward(180)
+forth_turtle.right(90)
+forth_turtle.forward(65)
+forth_turtle.left(90)
+forth_turtle.forward(100)
+
+turtle.mainloop()
